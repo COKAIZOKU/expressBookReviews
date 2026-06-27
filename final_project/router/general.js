522c7d469ca24c263f7b384192be98283ff68c8b
pvvegar <proteicode@proton.me>
2026-06-27 05:02:21 -0400

    refactor: axios is back

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios').default;
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -25,71 +26,89 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
-function getBooksPromise(bookData) {
-  return new Promise((resolve, reject) => {
-    if (bookData) {
-      resolve(bookData);
-    } else {
-      reject("No books found");
-    }
-  });
-}
-// Task 10: Get the list of all books using async/await
+const getBooksUrl = (req) => `${req.protocol}://${req.get('host')}/books`;
+
+// Local data endpoint used by Axios requests in Tasks 10-13.
+public_users.get('/books',function (req, res) {
+  res.status(200).json(books);
+});
+
+// Task 10: Get the list of all books using async/await with Axios
 public_users.get('/',async function (req, res) {
   try {
-    const bookList = await getBooksPromise(books);
-    res.send(bookList);
+    const response = await axios.get(getBooksUrl(req));
+    res.send(JSON.stringify(response.data,null,4));
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).json({message: "Unable to retrieve books"});
   }
 });
 
-// Task 11: Get book details based on ISBN using Promise callbacks
+// Task 11: Get book details based on ISBN using Promise callbacks with Axios
 public_users.get('/isbn/:isbn',function (req, res) {
   // Retrieve the isbn parameter from the request URL and send the corresponding book details
   const isbn = req.params.isbn;
 
-  getBooksPromise(books[isbn])
-    .then(result => res.send(result))
-    .catch(error => res.send(error));
+  axios.get(getBooksUrl(req))
+    .then(response => {
+      if (response.data[isbn]) {
+        res.send(response.data[isbn]);
+      } else {
+        res.status(404).json({message: "Book not found"});
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message: "Unable to retrieve book"});
+    });
  });
   
-// Task 12: Get book details based on author using Promise callbacks
+// Task 12: Get book details based on author using Promise callbacks with Axios
 public_users.get('/author/:author',function (req, res) {
   // Retrieve the author parameter from the request URL and send matching book details
-  const author = req.params.author;
+  const author = decodeURIComponent(req.params.author).toLowerCase();
   let result = {};
 
-  getBooksPromise(books)
-    .then(bookList => {
-      Object.keys(bookList).forEach((key) => {
-        if (bookList[key].author === author) {
-          result[key] = bookList[key];
+  axios.get(getBooksUrl(req))
+    .then(response => {
+      Object.keys(response.data).forEach((key) => {
+        if (response.data[key].author.toLowerCase() === author) {
+          result[key] = response.data[key];
         }
       });
 
-      res.send(result);
+      if (Object.keys(result).length > 0) {
+        res.send(result);
+      } else {
+        res.status(404).json({message: "Books by author not found"});
+      }
     })
-    .catch(error => res.send(error));
+    .catch(error => {
+      res.status(500).json({message: "Unable to retrieve books by author"});
+    });
 });
 
-// Task 13: Get book details based on title using Promise callbacks
+// Task 13: Get book details based on title using Promise callbacks with Axios
 public_users.get('/title/:title',function (req, res) {
   // Retrieve the title parameter from the request URL and send matching book details
-  const title = req.params.title;
+  const title = decodeURIComponent(req.params.title).toLowerCase();
   let result = {};
 
-  getBooksPromise(books)
-    .then(bookList => {
-      Object.keys(bookList).forEach((key) => {
-        if (bookList[key].title === title) {
-          result[key] = bookList[key];
+  axios.get(getBooksUrl(req))
+    .then(response => {
+      Object.keys(response.data).forEach((key) => {
+        if (response.data[key].title.toLowerCase() === title) {
+          result[key] = response.data[key];
         }
       });
 
-      res.send(result);
+      if (Object.keys(result).length > 0) {
+        res.send(result);
+      } else {
+        res.status(404).json({message: "Books by title not found"});
+      }
     })
-    .catch(error => res.send(error));
+    .catch(error => {
+      res.status(500).json({message: "Unable to retrieve books by title"});
+    });
 });
 
 //  Get book review
